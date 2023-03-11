9b3b0e695c52f281905217dab26b1c89a9feacca
ahmad amira <ahmad_amira@outlook.com>
2023-03-11 15:13:02 +0200

    Add components

---
+++ src/components/Middelcol/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./index.css";
+
+const Middelcol = () => {
+    return (
+        <div>
+            <div className="middel">
+                <div className="middel-header">
+                    <div className="middel-header1">
+                        <img
+                            src="img/icon clock.jpg"
+                            class="article-image"
+                        />
+                        <p>Inbox</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Middelcol
