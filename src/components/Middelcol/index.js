bbe809fa5d221f39e2d372e869f4a6f6f9d37451
ahmad amira <ahmad_amira@outlook.com>
2023-03-23 16:19:04 +0200

    added prps

--- src/components/Middelcol/index.js
+++ src/components/Middelcol/index.js
@@ -1,25 +1,26 @@
 import React from 'react'
+import CircleImg from '../MainSection/EmailContent/CircleImg/CircleImg';
 import Icon from '../MainSection/EmailContent/Icon/Icon';
 import "./index.css";
 import Middelcolhead from './Middelcolhead';
 import Middelitem from './Middelitem';
+import { data } from './Middelitem/data';
 
 
 const Middelcol = () => {
+
+    let y = <CircleImg name={"A"} color={"#8E562E"} />;
     return (
-        <div>
+
+        <div className='middelcol'>
             <Middelcolhead />
-            <Middelitem />
-            <Middelitem />
-            <Middelitem />
-            <p className='section'>April</p>
-            <Middelitem />
-            <p className='section'>Older</p>
-            <Middelitem />
-            <Middelitem />
-            <Middelitem />
-            <Middelitem />
-            <Middelitem />
+            {data.map((item, index) => (
+                <Middelitem {...item} key={index} active={index === 0 ? "active" : ""} >
+                    {index === 2 && <p className='section sepe'>April</p>}
+                    {index === 3 && <p className='section sepe'>Older</p>}
+                </Middelitem>
+            ))}
+
 
         </div>
     )
